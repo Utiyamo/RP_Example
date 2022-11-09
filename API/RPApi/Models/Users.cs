045a1e408bed84fd47443b3ec49ccd05ea045c01
Daniel Utiyama <47907956+Utiyamo@users.noreply.github.com>
2022-11-09 01:03:33 -0300

    Users Validation + JWT Token creation

---
+++ API/RPApi/Models/Users.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace RPApi.Models
+{
+    public class Users : BaseModel
+    {
+
+        [BsonId]
+        public ObjectId? Id { get; set; }
+
+        [BsonElement("Name")]
+        public string Name { get; set; }
+
+        [BsonElement("Username")]
+        public string Username { get; set; }
+
+        [BsonElement("Email")]
+        public string? Email { get; set; }
+
+        [BsonElement("Password")]
+        public string Password { get; set; }
+    }
+}
